f8742a183a3a7ea9f27658d66bfd0929065cfa15
Sven <67136317+Sven-Bo@users.noreply.github.com>
2022-08-14 10:03:23 +0200

    🎉 Initial Commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Merge & Save txt files to xlsm files
+This solution merges matching txt files from a given input folder and saves the result in an Excel file (macro enabled)
+
+## Problem Statement
+![Problem](/problemstatement_1.jpg?raw=true "Problem")
+![Problem](/problemstatement_2.jpg?raw=true "Problem")
+![Problem](/problemstatement_3.jpg?raw=true "Problem")
+![Problem](/problemstatement_4.jpg?raw=true "Problem")
+
+## Requirements
+Install the dependencies with pip
+```console
+pip install pandas
+pip install xlsxwriter
+```
+
+## About Me
+- Sven from Coding Is Fun
+- YouTube: https://youtube.com/c/CodingIsFun
+- Website: https://pythonandvba.com
+
+[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/X7X47Q0EG)
+
+## Feedback
+If you have any feedback, please reach out to me at contact@pythonandvba.com
+![Logo](https://www.pythonandvba.com/banner-img)
---
+++ generate_excel_files.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+import pandas as pd
+
+
+current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
+input_dir = current_dir / "INPUT"
+output_dir = current_dir / "OUTPUT"
+output_dir.mkdir(exist_ok=True, parents=True)
+files = list(input_dir.rglob("*.txt"))
+
+# The 'key' of 'Student_Class1A_23224_Eng.txt' is 'Student_Class1A_23224'
+keys = set("_".join(file.stem.split("_")[:3]) for file in files)
+
+for key in keys:
+    with pd.ExcelWriter(
+        output_dir / f"{key}_Results.xlsx", engine="xlsxwriter"
+    ) as writer:
+        for file in files:
+            if file.stem.startswith(key):
+                if file.stem.endswith("_Eng"):
+                    df_eng = pd.read_csv(file, sep="\t")
+                    df_eng.to_excel(writer, sheet_name="Test_Eng", index=False)
+                if file.stem.endswith("_Math"):
+                    df_math = pd.read_csv(file, sep="\t")
+                    df_math.to_excel(writer, sheet_name="Test_Math", index=False)
+        # Pandas can only export to xls, xlsx
+        # To export to xlsm, we need to inject a template vbaProject.bin using xlsxwriter
+        workbook = writer.book
+        workbook.filename = output_dir / f"{key}_Results.xlsm"
+        workbook.add_vba_project(current_dir / "vbaProject.bin")
+
+# Clean up the temp xlsx files in the output dir
+tmp_files = list(output_dir.rglob("*.xlsx"))
+for tmp_file in tmp_files:
+    tmp_file.unlink()
